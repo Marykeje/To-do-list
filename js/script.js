15fcb66b467778e28c81c9866e43b4d050606a1b
marykeje <marykbisong@gmail.com>
2025-09-21 17:56:12 +0100

    added js implementation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function cancel() {
+  document.getElementById("cancel").style.display="none";
+}
+document.getElementByIdby('modal');
+document.getElementById('hidden');
+document.getElementById('add')
+
