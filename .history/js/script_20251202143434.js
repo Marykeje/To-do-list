0221a99294ef006eac6c3ee0a5e1a40d91278969
Marykeje <marykbisong@gmail.com>
2025-12-04 10:28:45 +0100

    worked on the modal pop up and add button

---
+++ .history/js/script_20251202143434.js
@@ -0,0 +1,40 @@
+function cancel() {}
+
+// getting the elements
+// let cancel = (document.getElementById("cancel").style.display = "none");
+
+// let open = document.getElementById("add");
+// let modal = document.getElementByIdby("openModal");
+// let cancelModal = document.getElementById("cancelModal");
+
+// when button is opened
+// modal.addEventListener("click", () => {
+// open.style.display = ""
+// modal.style.display = "block"; // show modal
+// });
+
+// const openModal = function () {
+// console.log("Button clicked");
+// modal.classList.add("open"); //gives the list of css class in the element
+// };
+
+function openModal() {
+  const clickOpen = document.getElementById("add-btn");
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("closeOverlay");
+
+  const closeModal = document.getElementById("cancelModal");
+
+  clickOpen.style.display = "block";
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  // closeModal.style.display.hidden = "";
+
+  for the close modal
+  // function closeModal() {
+  // const close = document.getElementById("close");
+  // }
+  // console.log(modal.style.display)
+}
+
+// openModal()
---
+++ .history/js/script_20251203150736.js
@@ -0,0 +1,50 @@
+function cancel() {}
+
+// getting the elements
+// let cancel = (document.getElementById("cancel").style.display = "none");
+
+// let open = document.getElementById("add");
+// let modal = document.getElementByIdby("openModal");
+// let cancelModal = document.getElementById("cancelModal");
+
+// when button is opened
+// modal.addEventListener("click", () => {
+// open.style.display = ""
+// modal.style.display = "block"; // show modal
+// });
+
+
+
+// const openModal = function () {
+// console.log("Button clicked");
+// modal.classList.add("open"); //gives the list of css class in the element
+// };
+
+function openModal() {
+  const clickOpen = document.getElementById("add-btn");
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("closeOverlay");
+  const showDrop = document.getElementById("show-drop");
+
+  // const closeModal = document.getElementById("cancelModal");
+
+  clickOpen.style.display = "block";
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  showDrop.style.display = "blobk";
+  // closeModal.style.display.hidden = "";
+
+  // console.log(modal.style.display)
+}
+
+// openModal()
+// for the close modal
+function closeModal() {
+  const closeModal = document.getElementById("cancel-modal");
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("closeOverlay");
+
+  closeModal.style.display = "block";
+  modal.style.display = "none";
+  overlay.style.display = "none";
+}
